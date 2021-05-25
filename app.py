e89ea41924003b91c7c8c2dd2fa2bea1ae456e6a
NphtgDCw <tatyana.a.shevchenko@gmail.com>
2021-05-26 01:04:20 +0300

    remove Hello

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from flask_restful import Api
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
-# Route
-from resources.Hello import Hello
-api.add_resource(Hello, '/Hello')
 
 from resources.departments import DepartmentsResource
 api.add_resource(DepartmentsResource, '/departments')
