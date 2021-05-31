2c4b0402bae77affd761e717c5f8cf46b45f1164
NphtgDCw <tatyana.a.shevchenko@gmail.com>
2021-05-31 18:13:27 +0300

    add changes to travis config

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def create_app(config_filename):
     return app
 
 config_name = os.getenv('FLASK_CONFIG')
-app = create_app('production')
+app = create_app(config_name)
 
 if __name__ == "__main__":
     app.run(port='5002')
